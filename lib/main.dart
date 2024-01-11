c8cccd94db4bb0f300d796ba60bdca6a319db19b
Sabahmushtaq <sabahmushtaq002@gmail.com>
2024-01-11 22:15:36 +0530

    Final commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:istprojectlelafe/Models/PostModel.dart';
 import 'package:istprojectlelafe/search.dart';
 import 'package:istprojectlelafe/settings.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,12 +49,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
 var defaultValue="TOP STORIES";
 
+
 List <String>items=[
   "TOP STORIES",
   "LAST 24 HOURS","LAST 28 HOURS","LAST WEEK","NEW STORIES","BEST STORIES",
   "ASK HN","SHOW HN","HN JOBS","BOOKMARKS",
 
 ];
+var Loggedin;
+var namecontroller=TextEditingController();
+var emailcontroller=TextEditingController();
 List<PostModel> postList=[];
 Future<List<PostModel>> getPostApi ()async{
   final response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
@@ -103,92 +108,11 @@ bool _obscured=true;
                   Navigator.push(context, MaterialPageRoute(builder: (context)=> Search()));
                 },
                     child: Icon(Icons.search)),
-                    PopupMenuButton(
-                      itemBuilder: ( context)=>[
-                        PopupMenuItem(child: Text("Settings"),value: "set",),
-                        PopupMenuItem(child: Text("Login"),value: "login",),
-                      ],onSelected: (String newValue){
-                      if(newValue=="login"){
-
-                        showDialog(context: context,
-                            builder: (BuildContext context){
 
 
-
-                              return Center(
-      child: StatefulBuilder(
-      builder: (context, setState)
-      {
-        return SingleChildScrollView(
-
-          child: Container(
-            padding: EdgeInsets.all(8.0),
-
-            child: AlertDialog(
-             // insetPadding: EdgeInsets.symmetric(horizontal: 30, vertical: 150),
-
-              // buttonPadding: EdgeInsets.zero, //contentPadding: EdgeInsets.zero, // Customize padding to reduce height
-              title:
-              Text("LOGIN TO HACKER NEWS"),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(label: Text("USERNAME")),),
-                  TextField(
-
-                    obscureText: _obscured,
-                    decoration: InputDecoration(label: Text("PASSWORD",),
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            _obscured = !_obscured;
-                            // Prevents focus if tap on eye
-                          });
-                        },
-                        icon: Icon(
-                          _obscured ? Icons.visibility_off_rounded
-                              : Icons.visibility_rounded,
-                          size: 15,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Text(
-                      "YOUR INFORMATION IS ONLY SAVED LOCALLY AND USED FOR API CALLS"),
-                  OutlinedButton(
-                      onPressed: () {}, child: Text("how does it work"))
-                ],
-              ),
-              actions: [Row(mainAxisAlignment: MainAxisAlignment.end,children: [
-                OutlinedButton(onPressed: () {
-                  Navigator.pop(context);
-                }, child: Text("CANCEL")),
-                OutlinedButton(onPressed: () {}, child: Row(
-
-                  children: [
-                    Text("save", style: TextStyle(),),
-                    Icon(Icons.check)
-                  ],
-                )),
-              ],)
-
-              ],),
-          ),
-        );
-      }),
-    );
-
-                            }
-                            );
-                      }
-                      if(newValue=="set"){
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=> Settings()));
-                      }
-                    },
+                    Container(
+                      child: callhere(context),
                     )
-
-
                   ]),
             ),                          //in case of container and list ,wrap list with expanded
            /* ListView.builder(physics: NeverScrollableScrollPhysics(),shrinkWrap:true,itemBuilder: (context,index){
@@ -232,8 +156,172 @@ bool _obscured=true;
         ),
       )
     );
-  }//
-//
-//
+
+  }////preference
+void preference() async{
+  var sharedpref= await SharedPreferences.getInstance();
+  sharedpref.setString("name",namecontroller.toString());
+
+  sharedpref.setString("email",emailcontroller.toString());
+  sharedpref.setBool("login",true);
+  var getpref= await SharedPreferences.getInstance();
+  print("hi");
+  print("hello");
+  namecontroller.clear();
+  emailcontroller.clear();
+  Loggedin=getpref.getBool("login");
+  print("$Loggedin,same");
+  if(Loggedin!=null){
+    if(Loggedin==true){
+    print("h3");
+    Navigator.pop(context);
+
+//    callhere(context);
+
+
+     // Navigator.pop(context);
+    }
+
+  }
+
+}
+//void getpreference() async {}
+createmenuitems(){
+  return PopupMenuButton(
+
+
+    itemBuilder: ( context)=>[
+      PopupMenuItem(child: Text("Settings"), value: "set",),
+      PopupMenuItem(child: Text("Login"), value: "login",)
+    ],onSelected: (String newValue){
+    if(newValue=="login"){
+
+      showDialog(context: context,
+          builder: (BuildContext context){
+
+
+
+
+                    return StatefulBuilder(builder:(context,setStateSB) {
+                      return Center(
+                        child: SingleChildScrollView(
+
+                          child: Container(
+                            padding: EdgeInsets.all(8.0),
+
+                            child: AlertDialog(
+                              alignment: Alignment.center,
+                              // insetPadding: EdgeInsets.symmetric(horizontal: 30, vertical: 150),
+
+                              // buttonPadding: EdgeInsets.zero, //contentPadding: EdgeInsets.zero, // Customize padding to reduce height
+                              title:
+                              Text("LOGIN TO HACKER NEWS"),
+                              content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  TextField(
+                                    controller: namecontroller,
+                                    decoration: InputDecoration(
+                                        label: Text("USERNAME")),),
+                                  TextField(
+                                    controller: emailcontroller,
+                                    obscureText: _obscured,
+                                    decoration: InputDecoration(
+                                      label: Text("PASSWORD",),
+                                      suffixIcon: IconButton(
+                                        onPressed: () {
+                                          setStateSB(() {
+                                            _obscured = !_obscured;
+                                            // Prevents focus if tap on eye
+                                          });
+                                        },
+                                        icon: Icon(
+                                          _obscured ? Icons
+                                              .visibility_off_rounded
+                                              : Icons.visibility_rounded,
+                                          size: 15,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                      "YOUR INFORMATION IS ONLY SAVED LOCALLY AND USED FOR API CALLS"),
+                                  OutlinedButton(
+                                      onPressed: () {},
+                                      child: Text("how does it work"))
+                                ],
+                              ),
+                              actions: [Row(mainAxisAlignment: MainAxisAlignment
+                                  .end, children: [
+                                OutlinedButton(onPressed: () {
+                                  Navigator.pop(context);
+                                }, child: Text("CANCEL")),
+                                OutlinedButton(onPressed: () {
+                                  if (namecontroller.text.isNotEmpty &&
+                                      emailcontroller.text.isNotEmpty) {
+                                    preference();
+                                    setState(() {
+
+                                    });
+                                    //getpreference();
+                                    // callhere(context);
+                                  }
+                                }, child: Row(
+
+                                  children: [
+                                    Text("save", style: TextStyle(),),
+                                    Icon(Icons.check)
+                                  ],
+                                )),
+                              ],)
+
+                              ],),
+                          ),
+                        ),
+                      );
+                    });
+
+
+
+          }
+      );
+    }
+    if(newValue=="set"){
+      Navigator.push(context as BuildContext, MaterialPageRoute(builder: (context)=> Settings()));
+    }
+  },
+  );
+}
+
+  callhere(context)  {
+    if(Loggedin==null||Loggedin==false){
+    return createmenuitems();
+
+    }
+    else if(Loggedin==true){
+      return PopupMenuButton(itemBuilder: (context)=>
+         [ PopupMenuItem(child: Text("Logout"), value: "ret",),
+          PopupMenuItem(child: Text("SUBMIT"), value: "login",),
+           PopupMenuItem(child: Text("PROFILE"), value: "login",),
+           PopupMenuItem(child: Text("SETTINGS"), value: "login",),
+
+      ],onSelected: (String newvalue){
+        if(newvalue=="ret"){
+          Loggedin=false;
+          setState((){});
+
+
+
+
+
+
+        }
+      },
+      );
+    }
+
+
+  }
+
 
 }
